306982e2bda2c9af538deee35b36a99494460c9d
Yro <yruoan@outlook.com>
2022-11-11 16:49:54 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello GiT!");
         System.out.println("hello GiT2!");
+        System.out.println("hello GiT3!");
     }
 }
