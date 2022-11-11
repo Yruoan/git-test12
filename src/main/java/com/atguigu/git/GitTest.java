b2208ffbbc93b50da9ef19f15fa9f8f28ee63827
Yro <yruoan@outlook.com>
2022-11-11 17:08:04 +0800

    hot-fix test11

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello GiT4!");
         System.out.println("hello GiT5!");
         System.out.println("hot-test!");
+        System.out.println("hot-test!1");
     }
 }
