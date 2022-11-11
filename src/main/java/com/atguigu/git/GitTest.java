45a960f23ad5a57d8f43ac3adc0677f502e8c4b4
Yro <yruoan@outlook.com>
2022-11-11 16:58:31 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello GiT!");
         System.out.println("hello GiT2!");
         System.out.println("hello GiT3!");
+        System.out.println("hello GiT4!");
     }
 }
