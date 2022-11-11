323c79f7582155f8147b683bf73fd197029bcc1b
Yro <yruoan@outlook.com>
2022-11-11 16:48:59 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello GiT!");
+        System.out.println("hello GiT2!");
     }
 }
